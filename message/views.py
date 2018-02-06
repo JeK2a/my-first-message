19075ddc052e764300196bfa41566fec1f1287b5
JeK2a <vipjonpc@mail.ru>
2018-02-06 14:45:33 +0300

    Changed the HTML for the site.

--- message/views.py
+++ message/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Message
 
 
 def message_list(request):
-    return render(request, 'message/message_list.html', {})
+    messages = Message.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'message/message_list.html', {'messages': messages})
+
+
